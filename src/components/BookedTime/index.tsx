da292b2007a875ed8b3eb5604853d467d98770eb
Simon Ehleringer <simonehleringer@gmail.com>
2021-10-30 16:01:49 +0200

    Add duration to booked time

--- src/components/BookedTime/index.tsx
+++ src/components/BookedTime/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { TimeResponseModel } from '../../api/response-models';
+import TimeDisplayer from '../TimeDisplayer';
 import './style.css';
 
 interface BookedTimeProps {
@@ -12,11 +13,28 @@ const BookedTime: React.FC<BookedTimeProps> = ({ time }) => {
   const startTimeAsDate = new Date(startTime + 'Z');
   const endTimeAsDate = new Date(endTime + 'Z');
 
+  const durationInSeconds = Math.round(
+    (endTimeAsDate.getTime() - startTimeAsDate.getTime()) / 1000
+  );
+
   return (
     <div className='booked-time'>
-      <div>{id}</div>
-      <div>{startTimeAsDate.toLocaleString()}</div>
-      <div>{endTimeAsDate.toLocaleString()}</div>
+      <div>
+        No. <br />
+        {id}
+      </div>
+      <div>
+        Start: <br />
+        {startTimeAsDate.toLocaleString()}
+      </div>
+      <div>
+        End: <br />
+        {endTimeAsDate.toLocaleString()}
+      </div>
+      <div>
+        Duration:
+        <TimeDisplayer seconds={durationInSeconds} />
+      </div>
       <div>{description}</div>
     </div>
   );
